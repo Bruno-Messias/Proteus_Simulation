b690d4326fcfd8e56daced8eb53b2d10b9d2adeb
Bruno Cayres Messias <brunocmessias@hotmail.com>
2020-05-10 20:26:30 -0300

    repair bin folder

--- Arduino_DCmotor/bin/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-c:\Users\bruno\OneDrive\Documentos\GitHub\Proteus_Simulation\Arquivos\ .ino\bin\core\WString.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\WString.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\WString.h
--- Arduino_DCmotor/bin/core/wiring_pulse.S.d
+++
@@ -1,2 +0,0 @@
-c:\Users\bruno\OneDrive\Documentos\GitHub\Proteus_Simulation\Arquivos\ .ino\bin\core\wiring_pulse.S.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\wiring_pulse.S
